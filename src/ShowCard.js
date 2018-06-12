95d779702857acb6e469f83521b707588a2ea905
Brian Holt <btholt@gmail.com>
2018-06-12 16:05:36 -0700

    8

--- src/ShowCard.js
+++ src/ShowCard.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { string } from "prop-types";
 import styled from "react-emotion";
+import axios from "axios";
 
 const Wrapper = styled("div")`
   width: 32%;
@@ -11,19 +12,40 @@ const Wrapper = styled("div")`
   overflow: hidden;
 `;
 
-const ShowCard = props => (
-  <Wrapper>
-    <img
-      alt={`${props.title} show poster`}
-      src="http://placecorgi.com/400/400"
-    />
-    <div>
-      <h3>{props.title}</h3>
-      <h4>({props.year})</h4>
-      <p>{props.description}</p>
-    </div>
-  </Wrapper>
-);
+class ShowCard extends React.Component {
+  state = {
+    score: -1
+  };
+  componentDidMount() {
+    axios.get(`http://localhost:3000/${this.props.imdbID}`).then(response => {
+      this.setState({
+        score: response.data.rating
+      });
+    });
+  }
+  render() {
+    let score;
+    if (this.state.score < 0) {
+      score = <h4>loading …</h4>;
+    } else {
+      score = <h4>{this.state.score}</h4>;
+    }
+    return (
+      <Wrapper>
+        <img
+          alt={`${this.props.title} show poster`}
+          src="http://placecorgi.com/400/400"
+        />
+        <div>
+          <h3>{this.props.title}</h3>
+          <h4>({this.props.year})</h4>
+          {score}
+          <p>{this.props.description}</p>
+        </div>
+      </Wrapper>
+    );
+  }
+}
 
 ShowCard.propTypes = {
   title: string.isRequired,
